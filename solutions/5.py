e5ca24c134402f33cf44c8dc38788d0dfaa33623
Samuel Lijin <sxlijin@gmail.com>
2017-02-16 22:31:58 -0600

    remove more spaces

--- solutions/5.py
+++ solutions/5.py
@@ -4,5 +4,5 @@ def f(n):
  for i in p:
   if n == i: return (n,)
   if n % i == 0: return (i,)+f(n/i)
-def x(t,k=0):sp = ' '*k;return('%s<prime value="%d">\n%s</prime>' % (sp,t[0],sp)) if len(t) == 1 else ('%s<composite value="%d">\n%s\n%s\n%s</composite>'%(sp,reduce(mul,t,1),x(t[:1],k+2),x(t[1:],k+2),sp))
+def x(t,k=0):sp = ' '*k;return('%s<prime value="%d">\n%s</prime>'%(sp,t[0],sp)) if len(t)==1 else('%s<composite value="%d">\n%s\n%s\n%s</composite>'%(sp,reduce(mul,t,1),x(t[:1],k+2),x(t[1:],k+2),sp))
 for n in open('inputs/5.txt').readlines():print x(f(int(n)))
