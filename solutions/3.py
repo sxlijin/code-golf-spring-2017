d0d32490f7e6b8b7cc9bdbbdd35eb7f698d26570
Samuel Lijin <sxlijin@gmail.com>
2017-02-16 22:06:01 -0600

    strip some bytes #3

--- solutions/3.py
+++ solutions/3.py
@@ -1,12 +1,10 @@
 import itertools as i
 f=open('inputs/3.txt').readlines()
 w=set(w[:-1] for w in f)
-d ={a+b:(a,b) for a,b in i.permutations(w, 2)}
+d={a+b:(a,b) for a,b in i.permutations(w, 2)}
 r=set()
 for c in d:
-    if c in w:
-        r.add(c)
-        r.add(d[c][0])
-        r.add(d[c][1])
+ if c in w:
+  r.update((c,)+d[c])
 w.difference_update(r)
 print(w)
