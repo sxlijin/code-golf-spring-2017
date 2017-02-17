5e056230450fee1ca1b26c728e861f682f4f75fb
Samuel Lijin <sxlijin@gmail.com>
2017-02-16 22:09:51 -0600

    stip some more bytes

--- solutions/3.py
+++ solutions/3.py
@@ -1,5 +1,3 @@
 import itertools as i
-f=open('inputs/3.txt').readlines()
-w=set(l[:-1] for l in f)
-d={a+b:(a,b) for a,b in i.permutations(w, 2)}
-print '\n'.join(set().union(*(set(d[c]+(c,)) for c in d if c in w)))
+f=open('inputs/3.txt').readlines();s=set;w=s(l[:-1] for l in f);d={a+b:(a,b) for a,b in i.permutations(w,2)}
+print '\n'.join(s().union(*(s(d[c]+(c,)) for c in d if c in w)))
