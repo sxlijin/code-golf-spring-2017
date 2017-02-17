9e9d2812c883ecc4f1ed29ad995c2aad6253d44b
Samuel Lijin <sxlijin@gmail.com>
2017-02-16 22:31:10 -0600

    strip more spaces

--- solutions/5.py
+++ solutions/5.py
@@ -1,11 +1,8 @@
 from operator import mul
-r=range(2,41)
-p=[x for x in r if 0 not in (x%y for y in r if x!=y)]
+r=range(2,41);p=[x for x in r if 0 not in (x%y for y in r if x!=y)]
 def f(n):
  for i in p:
   if n == i: return (n,)
   if n % i == 0: return (i,)+f(n/i)
-
 def x(t,k=0): sp = ' '*k; return ('%s<prime value="%d">\n%s</prime>' % (sp, t[0], sp)) if len(t) == 1 else ('%s<composite value="%d">\n%s\n%s\n%s</composite>'%(sp, reduce(mul, t, 1), x(t[:1], k+2), x(t[1:], k+2), sp))
-
-for n in open('inputs/5.txt').readlines(): print x(f(int(n)))
+for n in open('inputs/5.txt').readlines():print x(f(int(n)))
