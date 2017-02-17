4c6eb33e50c00bce49397aeda68d07565100d9d4
Samuel Lijin <sxlijin@gmail.com>
2017-02-16 23:32:13 -0600

    fix solution to #3

--- solutions/3.py
+++ solutions/3.py
@@ -1,3 +1,6 @@
 import itertools as i
-f=open('inputs/3.txt').readlines();s=set;w=s(l[:-1] for l in f);d={a+b:(a,b) for a,b in i.permutations(w,2)}
-print '\n'.join(s().union(*(s(d[c]+(c,)) for c in d if c in w)))
+f=open('inputs/3.txt').readlines()
+s=set
+w=s(l[:-1] for l in f)
+d={a+b:(a,b) for a,b in i.permutations(w,2)}
+print w.difference(s().union(*(s(d[c]+(c,)) for c in d if c in w)))
