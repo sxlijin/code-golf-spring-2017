41ef861a6c10194f807f6e32bc24fc39ddf5d4e9
Samuel Lijin <sxlijin@gmail.com>
2017-02-16 22:30:16 -0600

    start compressing the file

--- solutions/5.py
+++ solutions/5.py
@@ -6,11 +6,6 @@ def f(n):
   if n == i: return (n,)
   if n % i == 0: return (i,)+f(n/i)
 
-def x(t,k=0):
-    sp = ' '*k
-    if len(t) == 1:
-        return ('%s<prime value="%d">\n%s</prime>' % (sp, t[0], sp))
-    else:
-        return ('%s<composite value="%d">\n%s\n%s\n%s</composite>'%(sp, reduce(mul, t, 1), x(t[:1], k+2), x(t[1:], k+2), sp))
+def x(t,k=0): sp = ' '*k; return ('%s<prime value="%d">\n%s</prime>' % (sp, t[0], sp)) if len(t) == 1 else ('%s<composite value="%d">\n%s\n%s\n%s</composite>'%(sp, reduce(mul, t, 1), x(t[:1], k+2), x(t[1:], k+2), sp))
 
 for n in open('inputs/5.txt').readlines(): print x(f(int(n)))
