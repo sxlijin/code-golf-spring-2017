d34544df18f17605d46097886685b64105e7400e
Samuel Lijin <sxlijin@gmail.com>
2017-02-16 21:51:05 -0600

    strip bytes from #2

--- solutions/2.py
+++ solutions/2.py
@@ -1,10 +1,8 @@
 n='1234567890'
-def parse(s):
-    t=[]
-    for ch in s:
-        t.append(ch)
-        while len(t) >= 3 and t[-3] in '+-*/' and t[-2] in n and t[-1] in n:
-            t[-3:] = [str(eval(t[-2]+t[-3]+t[-1]))]
-    return t[0]
-
-print '\n'.join(parse(l) for l in open('inputs/2.txt').readlines())
+def p(s):
+ t=[]
+ for ch in s:
+  t.append(ch)
+  while len(t) >= 3 and t[-3] in '+-*/' and t[-2] in n and t[-1] in n:t[-3:]=[str(eval(t[-2]+t[-3]+t[-1]))]
+ return t[0]
+print '\n'.join(p(l) for l in open('inputs/2.txt').readlines())
